58d870b27f36c5ed7ae935060efe3384af66ced6
Wen Liang Goh <wlgoh.2020@sis.smu.edu.sg>
2021-06-04 05:28:17 +0800

    q3 resource file and reformatting

--- resource/q3.c
+++ resource/q3.c
@@ -2,15 +2,15 @@
 
 
 int main(void) {
+
     int year;
-    printf("Enter year:");
+    printf("Enter year: ");
     scanf(" %d", &year);
 
-    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-    {
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
         printf("That's a leap year\n");
-    } else
-    {
+    } else {
         printf("That's not a leap year\n");
     }
+
 }
