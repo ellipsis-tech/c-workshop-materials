92b19c6c06b49b4507b0e519632c7bad9289f52c
michtyx <77957348+michtyx@users.noreply.github.com>
2021-06-03 15:57:34 +0800

    Add files via upload

---
+++ answers/q3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int year;
+    printf("Enter year:");
+    scanf(" %d", &year);
+
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    {
+        printf("That's a leap year\n");
+    } else
+    {
+        printf("That's not a leap year\n");
+    }
+}
